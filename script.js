7a27cfcc2ada037e227588b5169de9976419a33a
Anonypos <anonypos@hotmail.com>
2026-06-19 20:59:31 -0400

    Some updates :)

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+const home = document.getElementById("main_title");
+if(home){
+    home.addEventListener("click", ()=>{
+        
+    });
+}
+const paths = {"Hashtag maker" : "hashtag_maker",
+    "String to hex" : "string_to_hex",
+    "String duplicator" : "string_duplicator",
+    "String to binarry" : "string_to_binarry",
+    "String length counter" : "string_length_counter"
+}
 const tools_devs = document.body.getElementsByClassName("tool");
 for(i = 0; i < tools_devs.length; i++){
     const element = tools_devs[i];
+    const element_name = element.getElementsByClassName("tool_name")[0];
     element.addEventListener("mouseenter", ()=>{
         if(element){
             element.id = "element_selected";
@@ -13,4 +26,16 @@ for(i = 0; i < tools_devs.length; i++){
         }
 
     });
+    element.addEventListener("click",()=>{
+        if(element){
+            if(element_name){
+                if(paths[element_name.textContent]){
+                    window.open(paths[element_name.textContent])
+                }
+                else{
+                    alert("Coming soon!")
+                }
+            }
+        }
+    })
 }
